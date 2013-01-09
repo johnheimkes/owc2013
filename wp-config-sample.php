03569c7c4f6508270b907f589299ecd15ed3100f
John Heimkes IV <john@markupisart.com>
2013-01-08 18:17:10 -0600

    reconfigured wp-config-sample.php to have base creds and put local IP address in from John's home. The IP will change once at event

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,7 +20,7 @@
  * This will override the home/site URL settings
  * in the General Settings of wp-admin.
  */
-define('WP_HOME', 'http://[shenanigans.stuff]');
+define('WP_HOME', 'http://[shenanigans.local]');
 define('WP_SITEURL', WP_HOME);
 
 // ** MySQL settings - You can get this info from your web host ** //
@@ -34,7 +34,7 @@ define('DB_USER', 'owc');
 define('DB_PASSWORD', 'owc');
 
 /** MySQL hostname */
-define('DB_HOST', 'dev.papabear.me');
+define('DB_HOST', '10.0.1.13');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -51,14 +51,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         'W<e3v}L,!+0PE+59!z=RxT,y(Mr=x`2eb9$gO6p*TlfK?NK-5Wwjh4drD&tNlYbU');
+define('SECURE_AUTH_KEY',  'jL#_p>l|p|K+t$ +1>^-$x72II5+ISSPM!?Oll(~ZcU6~8Kadl;m<w9{1JH5 4P2');
+define('LOGGED_IN_KEY',    'e.8|;vF*mQ B[!MZhpR,c0}I rTtf|Gm/C[}*Z8&)j%EdF3o3NvMF)YWh,|o1xYk');
+define('NONCE_KEY',        '*]D6SxWWuE8]1Yc|+}Ud*^v8:Fzn9XC~DOg#5THp6a{-F::{&`[POMqwxHMC?C}w');
+define('AUTH_SALT',        'z=Oie>.-,,+Bh3[j;?wxWUjXa~MjJpbrkY7e&^h?x:5!w;+xo3P)(!5CF.Tt:SFU');
+define('SECURE_AUTH_SALT', '~kIID]:iwcw2+5{{8Jl)yI=%5!)+/dzBC=.J$sP+0i7@|b^abxs3F&L^{D18%~p-');
+define('LOGGED_IN_SALT',   './d>lo3YtLbjAJk_jAViZAwD[2-[H0rotCDxlVkZ,~-8[4&+@{a% +nr-]=_7gR8');
+define('NONCE_SALT',       ' ECz,-^tTC5^|a<4Hhf*FFt56z>r@5~3QY828&:,~!BLgcKOar^McBq-+Tzn0m8B');
 
 /**#@-*/
 
