d0a7ec8a8775f48932062c188019ee97a48e587b
Scott Garrison <sgarriso@nerdery.com>
2013-03-09 14:30:11 -0600

    Added some styles for our wireframing   fuck

---
+++ wp-content/themes/skeleton-theme/support-community.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Nerdery Theme
+ * template name: Support
+ * @category Nerdery_Skeleton_Theme
+ * @package Nerdery_Skeleton_Theme
+ * @subpackage Single
+ * @author
+ * @version $Id$
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="page-content">
+    <h2>Support Community</h2>
+    <div class="grid-row grid-row-push-s">
+        <div class="grid-col grid-col-5">
+            <h3>Help Us...</h3>
+            <p>
+                Bacon ipsum dolor sit amet excepteur aute reprehenderit turkey, ribeye laboris sed cow pork id ball tip fatback deserunt. Anim drumstick pork loin tri-tip ribeye labore chicken duis meatloaf tenderloin eu tongue shank elit meatball. Kielbasa fatback nisi, esse doner short loin leberkas ham ut aliqua ut velit beef ribs non pork loin. Chuck pork loin rump, commodo dolore reprehenderit andouille ea proident.
+            </p>
+        </div>
+        <div class="grid-col grid-col-5"><img src="http://placehold.it/960x500" /></div>
+    </div>
+    <div class="grid-row grid-row-push-s">
+        <div class="grid-col grid-col-10">
+            <img src="http://placehold.it/960x100" />
+        </div>
+    </div>
+    <div class="grid-row grid-row-push-s">
+        <div class="grid-col grid-col-5">
+            <h3>Donate Online</h3>
+        </div>
+        <div class="grid-col grid-col-5">
+            <h3>Donate Mail</h3>
+        </div>
+    </div>
+    <div class="grid-row">
+        <div class="grid-col grid-col-10">
+            <div class="grid-row">
+                <div class="grid-col grid-col-5">
+                    <ul class="h-list h-list-members">
+                        <li>
+                            Donator 1
+                        </li>
+                        <li>
+                            Donator 2
+                        </li>
+                        <li>
+                            Donator 3
+                        </li>
+                        <li>
+                            Donator 4
+                        </li>
+                        <li>
+                            Donator 5
+                        </li>
+                        <li>
+                            Donator 6
+                        </li>
+                    </ul>
+                </div>
+                <div class="grid-col grid-col-5">
+                    PRESIDENTS CIRCLE
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
+
+</body>
+</html>
