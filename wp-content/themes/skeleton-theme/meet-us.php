a9667ff491a1d13f5887178924578cc8e9c424dd
Kristine.Grad <grad.kristine@gmail.com>
2013-03-09 14:12:15 -0600

    Added Meet Us page

---
+++ wp-content/themes/skeleton-theme/meet-us.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Nerdery Theme
+ * template name: Meet-Us
+ * @category Nerdery_Skeleton_Theme
+ * @package Nerdery_Skeleton_Theme
+ * @subpackage Single
+ * @author
+ * @version $Id$
+ */
+?>
+<?php get_header(); ?>
+<div class="page-content style">
+    <h1>Meet Us</h1>
+        <div class="grid-row grid-row-push-s">
+            <div class="grid-col grid-col-10">
+                <img src="http://placekitten.com/960/300" />
+            </div>
+        </div>
+        <div class="grid-row grid-row-push-s">
+            <div class="grid-col grid-col-5">
+                About Statement
+            </div>
+            <div class="grid-col grid-col-5">
+                <img src="http://placekitten.com/480/300" />
+            </div>
+        </div>
+    <h1>Volunteers</h1>
+        <div class="grid-row grid-row-push-s">
+            <div class="grid-col grid-col-10">
+                <img src="http://placekitten.com/960/300" />
+            </div>
+        </div>
+        <div class="grid-row grid-row-push-s">
+            <div class="grid-col grid-col-5">About</div>
+            <div class="grid-col grid-col-5">Content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content</div>
+        </div>
+        <div class="grid-row grid-row-push-s">
+            <div class="grid-col grid-col-6">Grid Col 6</div>
+            <div class="grid-col grid-col-4">Grid Col 4</div>
+        </div>
+        <div class="grid-row grid-row-push-s">
+            <div class="grid-col grid-col-5">Grid Col 5</div>
+            <div class="grid-col grid-col-5">Grid Col 5</div>
+        </div>
+</div>
+
+<?php get_footer(); ?>
