2f211968bd05784a56051c6e93251a194555f6ba
Kristine.Grad <grad.kristine@gmail.com>
2013-03-10 04:50:46 -0500

    volunteer styling is good, minus the image

---
+++ wp-content/themes/skeleton-theme/volunteer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Nerdery Theme
+ * template name: Volunteer
+ * @category Nerdery_Skeleton_Theme
+ * @package Nerdery_Skeleton_Theme
+ * @subpackage Single
+ * @author
+ * @version $Id$
+ */
+?>
+<?php get_header(); ?>
+    <div class="section-wrap bg-lightblue">
+        <div class="grid-site">
+            <div class="grid-main">
+                <div class="grid-row-s">
+                    <div class="grid-col grid-col-5">
+                        <h2 class="hdg-5 hdg-bold">Volunteers</h3>
+                        <p>Dedicated and trained volunteers serve as the backbone of CMRS enabling us to remain closely connected to and representative of the communities we serve.  Because of their collective generous contributions of time and talent, lives are changed and communities transformed.</p>
+                    </div>
+                    <div class="grid-col grid-col-5">
+                        <img src="http://192.168.1.101/owc2013/wp-content/uploads/2013/03/about-hero.jpg" title="" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
