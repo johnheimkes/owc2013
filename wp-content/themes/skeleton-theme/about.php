f0b0c915c999f6c44eff9e1b4d84656191a369df
Kristine.Grad <grad.kristine@gmail.com>
2013-03-09 20:12:28 -0600

    Starting to rework about page

--- wp-content/themes/skeleton-theme/about.php
+++ wp-content/themes/skeleton-theme/about.php
@@ -11,39 +11,46 @@
 ?>
 <?php get_header(); ?>
 <div class="page-content style">
-    <h1>Mission</h1>
+    <h1>About</h1>
         <div class="grid-row grid-row-push-s">
-            <div class="grid-col grid-col-12">
-                "Mission Statement Here"
+            <div class="grid-col grid-col-2">
+                <div class="side-nav">
+                    Sidenav here
+                </div>
             </div>
+            <div class="grid-col grid-col-5">
+                <h3>Our Mission</h3>
+                <p>To develop the capacity of the community to respectfully resolve conflict and repair harm.</p>
+            </div>            
+            <div class="grid-col grid-col-4">
+                Picture here                
+            </div>              
         </div>
+
     <h1>Our Directors</h1>
         <div class="grid-row grid-row-push-s">
             <div class="grid-col grid-col-4">
-                <img src="http://placekitten.com/305" />
-                Name
+                First Last
+                <br />Title
             </div>
             <div class="grid-col grid-col-4">
-                <img src="http://placekitten.com/305" />
-                Name
+                First Last
+                <br />Title                
             </div>
             <div class="grid-col grid-col-4">
-                <img src="http://placekitten.com/305" />
-                Name
+                First Last
+                <br />Title                
             </div>                        
         </div>  
         <div class="grid-row grid-row-push-s">
             <div class="grid-col grid-col-4">
-                <img src="http://placekitten.com/305" />
-                Name
+                
             </div>
             <div class="grid-col grid-col-4">
-                <img src="http://placekitten.com/305" />
-                Name
+               
             </div>
             <div class="grid-col grid-col-4">
-                <img src="http://placekitten.com/305" />
-                Name
+                
             </div>                        
         </div> 
     <h1>At A Glance</h1>   
