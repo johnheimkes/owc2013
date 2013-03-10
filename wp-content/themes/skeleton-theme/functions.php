787017f0b1b6d726f505c2476fb8eb01f6af672e
John Heimkes IV <john@johnheimkes.com>
2013-03-09 19:16:50 -0600

    somehow I forgot to add support for fucking menus

--- wp-content/themes/skeleton-theme/functions.php
+++ wp-content/themes/skeleton-theme/functions.php
@@ -28,6 +28,7 @@ include_once 'widgets/skeleton-widget.php';
 add_theme_support('post-thumbnails');
 add_theme_support('custom-background');
 add_theme_support('custom-header');
+add_theme_support('menus');
 
 /**
  * Constants
