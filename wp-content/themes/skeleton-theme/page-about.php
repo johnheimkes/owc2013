d365dc7bb214e2b5dd59de4c024985d8458c69c2
Merne William <willi828@gmail.com>
2013-03-10 01:17:46 -0600

    about page.

--- wp-content/themes/skeleton-theme/page-about.php
+++ wp-content/themes/skeleton-theme/page-about.php
@@ -11,49 +11,75 @@
 ?>
 <?php get_header(); ?>
 
+ <!-- Our Staff -->
+<?php if(get_field('about__our_staff')): ?>
+    <ul>
+    <?php while(has_sub_field('about__our_staff')): ?>
+        <li>sub_field_1 = <?php the_sub_field('about__our_staff_content'); ?>, sub_field_2 = <?php the_sub_field('about__staff_image'); ?></li>
+    <?php endwhile; ?>
+    </ul>
+<?php endif; ?>
 
-<?php
 
-    // At a Glance
-    // At a Glance Title
-    the_field('about__glance_title');
-    // Num Community Services Med.
-    the_field('about__community_service_num');
-    // Num Court Services Med.
-    the_field('about__court_service_num');
-    // Num Youth and Restorative Med.
-    the_field('about__youth_restorative_num');
-    // Num Volunteer Training Med.
-    the_field('about__volunteer_training_num');
- 
-    // Financial Reports
-    // Income
-    // Total Income 
-    the_field('about__total_income');
-    // Income: Contracts and Fees 
-    the_field('about__income_contracts_and_fees');
-    // Income: Foundations
-    the_field('about__income_foundations');
-    // Income: State of MN
-    the_field('about__income_state_mn');
-    // Expenses
-    // Total Expenses
-    the_field('about__total_expenses');
-    // Expense: Program
-    the_field('about__expense_program');
-    // Expense: Fundraising
-    the_field('about__expense_fundraising');
-    // Expense: Administration
-    the_field('about__expense_admin');
-    
-    $rows = get_field('about__pdf_financial_report');
-    if($rows) {
-        echo '<ul>';
-        foreach($rows as $row) { ?>
-            <li>sub_field_1 =  <?php echo $row['about__finanical_report_title'] ?> | sub_field_2 = <?php echo $row['about__financial_report_pdf'] ?></li>';
-  <?php }
-        echo '</ul>';
-    }
- ?>
+<?php if(get_field('about__ft_staff')): ?>
+    <ul>
+    <?php while(has_sub_field('about__ft_staff')): ?>
+        <li>sub_field_1 = <?php the_sub_field('about__employee_title'); ?>, sub_field_2 = <?php the_sub_field('about__employee_name'); ?>, sub_field_1 = <?php the_sub_field('about__employee_biography'); ?></li>
+    <?php endwhile; ?>
+    </ul>
+<?php endif; ?>
+
+
+<?php if(get_field('about__pt_staff')): ?>
+    <ul>
+    <?php while(has_sub_field('about__pt_staff')): ?>
+        <li>sub_field_1 = <?php the_sub_field('about__pt_employee_name'); ?></li>
+    <?php endwhile; ?>
+    </ul>
+<?php endif; ?>
+
+
+<?php if(get_field('about__board_of_directors_officers')): ?>
+    <ul>
+    <?php while(has_sub_field('about__board_of_directors_officers')): ?>
+        <li>sub_field_1 = <?php the_sub_field('about__officer_name'); ?>, sub_field_2 = <?php the_sub_field('about__officer_title'); ?></li>
+    <?php endwhile; ?>
+    </ul>
+<?php endif; ?>
+
+
+<?php if(get_field('about__board_of_directors_members')): ?>
+    <ul>
+    <?php while(has_sub_field('about__board_of_directors_members')): ?>
+        <li>sub_field_1 = <?php the_sub_field('member_name'); ?></li>
+    <?php endwhile; ?>
+    </ul>
+<?php endif; ?>
+
+<?php if(get_field('about__accomplishments')): ?>
+    <ul>
+    <?php while(has_sub_field('about__accomplishments')): ?>
+        <li>sub_field_1 = <?php the_sub_field('about__acc_title'); ?>, sub_field_2 = <?php the_sub_field('about__community_service_num'); ?>, sub_field_3 = <?php the_sub_field('about__court_service_num'); ?>, sub_field_4 = <?php the_sub_field('about__youth_restorative_num'); ?>, sub_field_5 = <?php the_sub_field('about__volunteer_training_num'); ?>, sub_field_5 = <?php the_sub_field('about__accomplishments_pdf'); ?></li>
+    <?php endwhile; ?>
+    </ul>
+<?php endif; ?>
+
+<?php if(get_field('about__financial_reports_section')): ?>
+    <ul>
+    <?php while(has_sub_field('about__financial_reports_section')): ?>
+        <li>sub_field_1 = <?php the_sub_field('about__total_income'); ?>, sub_field_2 = <?php the_sub_field('about__income_contracts_and_fees'); ?>, sub_field_3 = <?php the_sub_field('about__income_foundations'); ?>, sub_field_4 = <?php the_sub_field('about__income_state_mn'); ?>, sub_field_5 = <?php the_sub_field('about__total_expenses'); ?>, sub_field_5 = <?php the_sub_field('about__expense_program'); ?>, sub_field_5 = <?php the_sub_field('about__expense_fundraising'); ?>, sub_field_5 = <?php the_sub_field('about__expense_admin'); ?>, sub_field_5 = <?php the_sub_field('about__percent_donate'); ?>, sub_field_5 = <?php the_sub_field('about__pdf_financial_report'); ?></li>
+    <?php endwhile; ?>
+    </ul>
+<?php endif; ?>
+
+
+<?php if(get_field('about__pdf_financial_report')): ?>
+    <ul>
+    <?php while(has_sub_field('about__pdf_financial_report')): ?>
+        <li>sub_field_1 = <?php the_sub_field('about__finanical_report_title'); ?>, sub_field_2 = <?php the_sub_field('about__financial_report_pdf'); ?></li>
+    <?php endwhile; ?>
+    </ul>
+<?php endif; ?>about__pdf_financial_report
+
 
 <?php get_footer(); ?>
