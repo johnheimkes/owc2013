9fb92278040119ca1e246dde5636eb5391f341fd
Kristine.Grad <grad.kristine@gmail.com>
2013-03-10 05:45:34 -0500

    most of training styles are done, updated about too

--- wp-content/themes/skeleton-theme/training.php
+++ wp-content/themes/skeleton-theme/training.php
@@ -11,14 +11,14 @@
 ?>
 <?php get_header(); ?>
 <div class="page style">
-    <h1>Training</h1>
     <div class="section-wrap section-wrap-about bg-lightblue">
         <div class="grid-site">
             <div class="grid-row">
                 <div class="grid-col grid-col-6">
                     <div class="hero-message">
                         <h2 class="hdg-4">Training</h1>
-                        <p>Community Mediation & Restorative Services, Inc believes training is an effective strategy as we seek to develop the capacity of our community to respectfully resolve conflict and repair harm. We collaborate with other mediation and restorative parties to create more opportunities for people to participate in transformative, skill-building experiences.</p>
+                            <p class="padded-bottom">Community Mediation & Restorative Services, Inc believes training is an effective strategy as we seek to develop the capacity of our community to respectfully resolve conflict and repair harm. </p>
+                            <p>We collaborate with other mediation and restorative parties to create more opportunities for people to participate in transformative, skill-building experiences.</p>
                     </div>
                 </div>
                 <div class="grid-col grid-col-6 padded-bottom">
@@ -27,49 +27,58 @@
             </div>
         </div>
     </div>
-    <div class="section-wrap section-wrap-about bg-grey">
+    <div class="section-wrap section-wrap-about bg-turq" style="margin-top: 50px">
         <div class="grid-site">
             <div class="grid-row">
-                <div class="hr">
-                    <div class="grid-col grid-col-8">
-                        <h2>Sign up to be notified</h2>
-                        <h3>
-                            30-Hour Basic Mediation and Communication Skills Training
-                        </h3>
-                        <p>
-                             Meets Rule 114 requirements for the Supreme Court Mediator Roster
-                        </p>
-                        <ul>
-                            Topics Include:
-                            <li>Conflict resolution and mediation theory</li>
-                            <li>Mediation skills and techniques</li>
-                            <li>Components in the mediation process</li>
-                            <li>Mediator conduct</li>
-                            <li>Mediation Rules, statutes, and practices</li>
-                            <li>Mediating in court, community, and school settings</li>
-                        </ul>
+                <div class="grid-col grid-col-9 white">
+                    <h3 class="hdg-4 hdg-bold">Sign up to be notified for the next available classes</h3>
+                </div>
+                <div class="grid-col grid-col-2 white">
+                    <div class="btn">
+                        Sign Up
                     </div>
-                <div class="grid-col grid-col-3">           
-                    Enter Email
-                </div>   
-                <div class="grid-col grid-col-1">           
-                    Send
-                </div>                                    
+                </div>                
             </div>
+        </div>
+    </div>    
+    <div class="section-wrap section-wrap-about bg-lightblue">
+        <div class="grid-site">
+            <div class="grid-row">
+                <div class="grid-col grid-col-12">
+                    <h3 class="hdg-5 hdg-bold darkblue">
+                        30-Hour Basic Mediation and Communication Skills Training
+                    </h3>
+                    <p class="padded">
+                         Meets Rule 114 requirements for the Supreme Court Mediator Roster
+                    </p>
+                    <ul.circle>
+                        <span class="hdg-bold">Topics Include:</span>
+                        <li>Conflict resolution and mediation theory</li>
+                        <li>Mediation skills and techniques</li>
+                        <li>Components in the mediation process</li>
+                        <li>Mediator conduct</li>
+                        <li>Mediation Rules, statutes, and practices</li>
+                        <li>Mediating in court, community, and school settings</li>
+                    </ul>
+                    <div class="btn" style="margin-top: 20px">
+                        Download PDF
+                    </div>
+                </div> 
+            </div>                         
             <div class="grid-row">            
-                <div class="grid-col grid-col-10">
-                    <h2>21 Hour Family Bridge Mediation Training Including 6 Hour Domestic Violence Training</h2>
+                <div class="grid-col grid-col-9 padded">
+                    <h2 class="hdg-5 hdg-bold darkblue">21 Hour Family Bridge Mediation Training Including 6 Hour Domestic Violence Training</h2>
                     <p>
                         Meets Rule 114 Family Bridge and 6 Hours of Domestic Violence Training CLE credits to be applied for 
                     </p>
                 </div>
-                <div class="grid-col grid-col-2">     
+                <div class="grid-col grid-col-3 padded">     
                     <div class="btn">Sign up for classes available</div>
                 </div>
             </div>
             <div class="grid-row">                                 
-                <div class="grid-col grid-col-11">
-                    <h2>14 Hour Family Bridge Mediation Training </h2>
+                <div class="grid-col grid-col-11 padded">
+                    <h2 class="hdg-5 hdg-bold darkblue">14 Hour Family Bridge Mediation Training </h2>
                     <p>
                         Meets Rule 114 Family Bridge and 6 Hours of Domestic Violence Training CLE credits to be applied for 
                     </p>
@@ -79,10 +88,10 @@
                 </div>   
             </div>
             <div class="grid-row">                           
-                <div class="grid-col grid-col-11">
-                    <h2>Restorative Practices</h2>
+                <div class="grid-col grid-col-11 padded">
+                    <h2 class="hdg-5 hdg-bold darkblue">Restorative Practices</h2>
                     <p>
-                        We provide a variety of restorative practice training opportunities including group and family conferencing and restorative circles. Restorative practices:
+                       We provide a variety of restorative practice training opportunities including group and family conferencing and restorative circles. Restorative practices:
                     </p>
                 </div>
                 <div class="grid-col grid-col-1">
@@ -90,10 +99,10 @@
                 </div>   
             </div>
             <div class="grid-row">                             
-                <div class="grid-col grid-col-11">
-                    <h2>Mediator Ethics & Statutes</h2>
+                <div class="grid-col grid-col-11 padded">
+                    <h2 class="hdg-5 hdg-bold darkblue">Mediator Ethics & Statutes</h2>
                     <p>
-                        A mix of discussion and experiential exercises designed to refresh your understanding of Rule 114 Code of Ethics and CMS best practices. 
+                       A mix of discussion and experiential exercises designed to refresh your understanding of Rule 114 Code of Ethics and CMS best practices. 
                     </p>
                 </div>
                 <div class="grid-col grid-col-1">
@@ -101,8 +110,8 @@
                 </div> 
             </div> 
             <div class="grid-row">                                   
-                <div class="grid-col grid-col-11">
-                    <h2>Continuing Education</h2>
+                <div class="grid-col grid-col-11 padded">
+                    <h2 class="hdg-5 hdg-bold darkblue">Continuing Education</h2>
                     <p>
                         CMRS directly and indirectly offers a variety of continuing education opportunities each year, including a 6-hour conference every Fall. 
                     </p>
@@ -113,6 +122,45 @@
             </div>
         </div>
     </div>    
+    <div class="section-wrap section-wrap-about bg-white">
+        <div class="grid-site">
+            <div class="grid-row">
+                <div class="grid-col grid-col-6">
+                    <h3 class="hdg-4 hdg-bold turq">Scholarships</h3>
+                    <br>Something accessible to all.
+
+                </div>
+                <div class="grid-col grid-col-6">
+                    <p>Phone: 555-555-5555  |  Email: hello@email.com</p>
+                </div>                
+            </div>
+        </div>
+    </div> 
+    <div class="section-wrap section-wrap-about bg-turq">
+        <div class="grid-site">
+            <div class="grid-row">
+                <div class="grid-col grid-col-12">
+                    <h3 class="hdg-4 hdg-bold">Activities Outside of Our Training</h3>
+                </div>
+            </div>
+            <div class="grid-row">
+                <div class="grid-col grid-col-6">
+                    <a href="#">Outside Event Link</a>
+                </div>     
+                <div class="grid-col grid-col-6">
+                    <a href="#">Outside Event Link</a>
+                </div>  
+            </div>
+            <div class="grid-row">
+                <div class="grid-col grid-col-6">
+                    <a href="#">Outside Event Link</a>
+                </div>     
+                <div class="grid-col grid-col-6">
+                    <a href="#">Outside Event Link</a>
+                </div>                                                                
+            </div>
+        </div>
+    </div>           
 </div>
 
 <?php get_footer(); ?>
