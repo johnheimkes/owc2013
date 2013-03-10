cf4cebe405588a04080969672bf7f4b468476516
Scott Garrison <sgarriso@nerdery.com>
2013-03-10 00:23:37 -0600

    updated footer

--- wp-content/themes/skeleton-theme/footer.php
+++ wp-content/themes/skeleton-theme/footer.php
@@ -10,26 +10,28 @@
  */
 ?>
         <div class="page-footer">
-        	<ul class="h-list nav-footer">
-        		<li>
-        			<a href="#">Link</a>
-        		</li>
-        		<li>
-        			<a href="#">Link</a>
-        		</li>
-        		<li>
-        			<a href="#">Link</a>
-        		</li>
-        		<li>
-        			<a href="#">Link</a>
-        		</li>
-        		<li>
-        			<a href="#">Link</a>
-        		</li>
-        		<li>
-        			<a href="#">Link</a>
-        		</li>
-        	</ul>
+            <div class="wrapper-footer">
+            	<ul class="nav-footer">
+            		<li>
+            			<a href="#">Link</a>
+            		</li>
+            		<li>
+            			<a href="#">Link</a>
+            		</li>
+            		<li>
+            			<a href="#">Link</a>
+            		</li>
+            		<li>
+            			<a href="#">Link</a>
+            		</li>
+            		<li>
+            			<a href="#">Link</a>
+            		</li>
+            		<li>
+            			<a href="#">Link</a>
+            		</li>
+            	</ul>
+            </div>
         </div>
     </div><!-- /.page-content -->
 <?php wp_footer(); ?>
