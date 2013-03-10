ccf0c81f3dda043a8891b41fb638986e8ebcc175
Scott Garrison <sgarriso@nerdery.com>
2013-03-10 06:03:42 -0500

    whatupp homepage changes

--- wp-content/themes/skeleton-theme/footer.php
+++ wp-content/themes/skeleton-theme/footer.php
@@ -11,26 +11,63 @@
 ?>
         <div class="page-footer">
             <div class="wrapper-footer">
-            	<ul class="nav-footer">
-            		<li>
-            			<a href="#">Link</a>
-            		</li>
-            		<li>
-            			<a href="#">Link</a>
-            		</li>
-            		<li>
-            			<a href="#">Link</a>
-            		</li>
-            		<li>
-            			<a href="#">Link</a>
-            		</li>
-            		<li>
-            			<a href="#">Link</a>
-            		</li>
-            		<li>
-            			<a href="#">Link</a>
-            		</li>
-            	</ul>
+                <div class="wrapper-nav-footer">
+                	<ul class="nav-footer">
+                		<li>
+                			<a href="#">Home</a>
+                		</li>
+                		<li>
+                			<a href="#">Mediation</a>
+                		</li>
+                		<li>
+                			<a href="#">Training</a>
+                		</li>
+                		<li>
+                			<a href="#">Volunteer</a>
+                		</li>
+                		<li>
+                			<a href="#">Support</a>
+                		</li>
+                		<li>
+                			<a href="#">About</a>
+                		</li>
+                        <li>
+                            <a href="#">Resources</a>
+                        </li>
+                        <li>
+                            <a href="#">Volunteer Login</a>
+                        </li>
+                	</ul>
+                </div>
+                <div class="footer-copy">
+                    <ul class="h-list">
+                        <li>
+                            &copy; 2013 Community Mediation &amp; Restorative Services Inc., all rights reserved
+                        </li>
+                        <li>
+                            <a href="#">Legal</a>
+                        </li>
+                        <li>
+                            <a href="#">Privacy Policy</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer-contact">
+                    <ul class="h-list">
+                        <li>
+                            Phone: 763-561-0033
+                        </li>
+                        <li>
+                            Espanol: 612-629-6058
+                        </li>
+                        <li>
+                            Fax: 763-561-0266
+                        </li>
+                        <li>
+                           <a href="mailto:staff@mediationprogram.com">staff@mediationprogram.com</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div><!-- /.page-content -->
