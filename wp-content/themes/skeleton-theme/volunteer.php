0446f55d0fcf54b08cea3ec91bd4b007cd76029a
Kristine.Grad <grad.kristine@gmail.com>
2013-03-10 06:08:32 -0500

    Weird Volunteer.php merge

--- wp-content/themes/skeleton-theme/volunteer.php
+++ wp-content/themes/skeleton-theme/volunteer.php
@@ -9,21 +9,59 @@
  * @version $Id$
  */
 ?>
-<?php get_header(); ?>
-    <div class="section-wrap bg-lightblue">
-        <div class="grid-site">
-            <div class="grid-main">
-                <div class="grid-row-s">
-                    <div class="grid-col grid-col-5">
-                        <h2 class="hdg-5 hdg-bold">Volunteers</h3>
-                        <p>Dedicated and trained volunteers serve as the backbone of CMRS enabling us to remain closely connected to and representative of the communities we serve.  Because of their collective generous contributions of time and talent, lives are changed and communities transformed.</p>
-                    </div>
-                    <div class="grid-col grid-col-5">
-                        <img src="http://192.168.1.101/owc2013/wp-content/uploads/2013/03/about-hero.jpg" title="" alt="" />
+<?php
+
+get_header();
+
+if ( have_posts() ) : while ( have_posts() ) : the_post();
+    
+?>
+<div class="section-wrap bg-lightblue">
+    <div class="grid-site">
+        <div class="grid-main">
+            <div class="grid-row-s">
+                <div class="grid-col grid-col-5">
+                    <h2 class="hdg-5 hdg-bold"><?php the_title(); ?></h3>
+                        <?php the_content(); ?>
+                    <br>             
+                    <div class="btn">
+                        Sign Up
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+<div class="section-wrap bg-grey">    
+    <div class="grid-site">
+        <div class="grid-main">        
+            <div class="grid-row-s">
+                <div class="grid-col grid-col-5">
+                    <h2 class="hdg-bold">There are five different types of volunteers:</h2>
+                    <?php if (get_field('volunteer_types')) : ?>
+                        <ul>
+                            <?php while(has_sub_field('volunteer_types')) : ?>
+                                <li><?php the_sub_field('volunteer_type'); ?></li>
+                            <?php endwhile; ?>
+                        </ul>
+                    <?php endif; ?>
+                </div>
+                <div class="grid-col grid-col-5">
+                    <p class="padded-bottom">How would YOU like to be involved?  When you’re ready (or even close to being ready) we’d love to hear from you. </p>
+                    <div class="btn">
+                        Sign Up
+                    </div>                        
+                </div>                    
+            </div>
+        </div>
+    </div>
+</div>
+    
 
-<?php get_footer(); ?>
+<?php
+
+endwhile; endif;
+
+get_footer();
+
+?>
