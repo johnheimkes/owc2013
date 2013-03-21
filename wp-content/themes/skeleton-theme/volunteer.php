cd67c407b5b68f7ca0a7561bda5b072cd6f747eb
Scott Garrison <sgarriso@nerdery.com>
2013-03-21 16:33:02 -0500

    updated the Volunteer page styling, looks to be fairly FED complete

--- wp-content/themes/skeleton-theme/volunteer.php
+++ wp-content/themes/skeleton-theme/volunteer.php
@@ -21,15 +21,22 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
         <div class="grid-main">
             <div class="grid-row-s">
                 <div class="grid-col grid-col-7">
-                    <h2 class="hdg hdg-5 hdg-bold"><?php the_title(); ?></h3>
-                        <span class="hdg-4 hdg"><?php the_content(); ?></span>
-                    <br>             
-                    <div class="btn">
-                        Sign Up
+                    <div class="feature">
+                        <div class="feature-hd">
+                            <h2 class="hdg hdg-5 hdg-bold"><?php the_title(); ?></h2>
+                        </div>
+                        <div class="feature-bd feature-bd_extended">
+                            <span class=""><?php the_content(); ?></span>
+                        </div>
+                        <div class="featured-ft">
+                            <a href="#" class="btn">Sign Up</a>
+                        </div>
                     </div>
                 </div>
                 <div class="grid-col grid-col-3">
-                    <img src="<?php bloginfo('template_directory'); ?>/assets/images/volunteersGraphic.png" alt="" />
+                    <div class="volunteer-img">
+                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/volunteersGraphic.png" alt="" />
+                    </div>
                 </div>
             </div>
         </div>
@@ -37,10 +44,14 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 </div>
 <div class="section-wrap bg-grey">    
     <div class="grid-site">
-        <div class="grid-main">        
+        <div class="grid-main">
+            <div class="grid-row-s grid-row-push-s">
+                <div class="grid-col grid-col-10">
+                    <h2 class="hdg-bold hdg-5">There are five different types of volunteers:</h2>
+                </div>
+            </div>        
             <div class="grid-row-s">
                 <div class="grid-col grid-col-5">
-                    <h2 class="hdg-bold">There are five different types of volunteers:</h2>
                     <?php if (get_field('volunteer_types')) : ?>
                         <ul>
                             <?php while(has_sub_field('volunteer_types')) : ?>
