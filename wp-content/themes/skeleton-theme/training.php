263a1df202c7caf1ac0a708664f78f93614a9ad8
Scott Garrison <sgarriso@nerdery.com>
2013-03-21 23:08:05 -0500

    updated a whole bunch of shit to the training page, 95% FED done

--- wp-content/themes/skeleton-theme/training.php
+++ wp-content/themes/skeleton-theme/training.php
@@ -33,92 +33,219 @@
         </div>
     </div>
    
-    <div class="section-wrap">
-        <div class="grid-site">
+    <div class="section-wrap bg-turq">
+        <div class="grid-site grid-site_thin">
             <div class="grid-main">
-                <div class="grid-row">
-                    <h3 class="hdg-4 hdg-bold">Sign up to be notified for the next available classes</h3>
-                </div>
-                <div class="grid-row">
-                    <div class="grid-col grid-col-12">
-                        <h3 class="hdg-5 hdg-bold darkblue">
-                            30-Hour Basic Mediation and Communication Skills Training
-                        </h3>
-                        <p class="">
-                             Meets Rule 114 requirements for the Supreme Court Mediator Roster
-                        </p>
-                        <ul>
-                            <span class="hdg-bold">Topics Include:</span>
-                            <li>Conflict resolution and mediation theory</li>
-                            <li>Mediation skills and techniques</li>
-                            <li>Components in the mediation process</li>
-                            <li>Mediator conduct</li>
-                            <li>Mediation Rules, statutes, and practices</li>
-                            <li>Mediating in court, community, and school settings</li>
-                        </ul>
-                        <div class="btn" style="margin-top: 20px">
-                            Download PDF
-                        </div>
-                    </div> 
-                </div>                         
-                <div class="grid-row">            
-                    <div class="grid-col grid-col-9 ">
-                        <h2 class="hdg-5 hdg-bold darkblue">21 Hour Family Bridge Mediation Training Including 6 Hour Domestic Violence Training</h2>
-                        <p>
-                            Meets Rule 114 Family Bridge and 6 Hours of Domestic Violence Training CLE credits to be applied for 
-                        </p>
+                <div class="grid-row-s">
+                    <div class="grid-col grid-col-8 white">
+                        <h2 class="hdg-4 hdg-bold">Sign up to be notified for the next available classes</h2>
                     </div>
-                    <div class="grid-col grid-col-3 ">     
-                        <div class="btn">Sign up for classes available</div>
+                    <div class="grid-col grid-col-2 white">
+                        <a href="#" class="btn">Sign Up</a>
                     </div>
-                </div>
-                <div class="grid-row">                                 
-                    <div class="grid-col grid-col-11 ">
-                        <h2 class="hdg-5 hdg-bold darkblue">14 Hour Family Bridge Mediation Training </h2>
-                        <p>
-                            Meets Rule 114 Family Bridge and 6 Hours of Domestic Violence Training CLE credits to be applied for 
-                        </p>
-                    </div>  
-                    <div class="grid-col grid-col-1">
-                        details
-                    </div>   
-                </div>
-                <div class="grid-row">                           
-                    <div class="grid-col grid-col-11 ">
-                        <h2 class="hdg-5 hdg-bold darkblue">Restorative Practices</h2>
-                        <p>
-                           We provide a variety of restorative practice training opportunities including group and family conferencing and restorative circles. Restorative practices:
-                        </p>
-                    </div>
-                    <div class="grid-col grid-col-1">
-                        details
-                    </div>   
-                </div>
-                <div class="grid-row">                             
-                    <div class="grid-col grid-col-11 ">
-                        <h2 class="hdg-5 hdg-bold darkblue">Mediator Ethics & Statutes</h2>
-                        <p>
-                           A mix of discussion and experiential exercises designed to refresh your understanding of Rule 114 Code of Ethics and CMS best practices. 
-                        </p>
-                    </div>
-                    <div class="grid-col grid-col-1">
-                        details
-                    </div> 
-                </div> 
-                <div class="grid-row">                                   
-                    <div class="grid-col grid-col-11 ">
-                        <h2 class="hdg-5 hdg-bold darkblue">Continuing Education</h2>
-                        <p>
-                            CMRS directly and indirectly offers a variety of continuing education opportunities each year, including a 6-hour conference every Fall. 
-                        </p>
-                    </div>
-                    <div class="grid-col grid-col-1">
-                        details
-                    </div>                                                                                                  
                 </div>
             </div>
         </div>
-    </div>            
+    </div>
+
+    <div class="section-wrap bg-lightblue">
+        <div class="grid-site">
+            <div class="grid-main">
+
+                <div class="training">
+                    <div class="training-hd">
+                        <div class="stretch">
+                            <h3 class="hdg-5 hdg-bold darkblue grid-col-8">
+                                30-Hour Basic Mediation and Communication Skills Training
+                            </h3>
+                            <span class="">Details</span>
+                        </div>
+                    </div>
+                    <div class="training-subhead grid-col-8">
+                        Meets Rule 114 requirements for the Supreme Court Mediator Roster
+                    </div>
+                    <div class="training-bd">
+                        <div class="grid-row-s">
+                            <div class="grid-col grid-col-5">
+                                <h4 class="hdg-bold">Topics Include:</h4>
+                                <ul class="training-list">
+                                    <li>Conflict resolution and mediation theory</li>
+                                    <li>Mediation skills and techniques</li>
+                                    <li>Components in the mediation process</li>
+                                    <li>Mediator conduct</li>
+                                    <li>Mediation Rules, statutes, and practices</li>
+                                    <li>Mediating in court, community, and school settings</li>
+                                </ul>
+                                <a class="btn">Download PDF</a>
+                            </div>
+                            <div class="grid-col grid-col-5">
+                                <h4 class="hdg-bold">Prices</h4>
+                                <ul class="training-table">
+                                    <li>
+                                        <table class="training-prices">
+                                            <tbody>
+                                                <tr>
+                                                    <td class="training-prices_category">
+                                                        Attorney (CIE Credits)
+                                                        <span class="training-prices_price">$600</span>
+                                                    </td>
+                                                    <td>
+                                                        <a href="#" class="btn">Register &amp; Buy</a>
+                                                    </td>
+                                                    <td>
+                                                        <a href="#" class="btn">Register</a>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </li>
+                                    <li>
+                                        <table class="training-prices">
+                                            <tbody>
+                                                <tr>
+                                                    <td class="training-prices_category">
+                                                        Private Practitioner
+                                                        <span class="training-prices_price">$350</span>
+                                                    </td>
+                                                    <td>
+                                                        <a href="#" class="btn">Register &amp; Buy</a>
+                                                    </td>
+                                                    <td>
+                                                        <a href="#" class="btn">Register</a>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </li>
+                                    <li>
+                                        <table class="training-prices">
+                                            <tbody>
+                                                <tr>
+                                                    <td class="training-prices_category">
+                                                        Volunteer
+                                                        <span class="training-prices_price">$200</span>
+                                                    </td>
+                                                    <td>
+                                                        <a href="#" class="btn">Register &amp; Buy</a>
+                                                    </td>
+                                                    <td>
+                                                        <a href="#" class="btn">Register</a>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div> 
+                </div>
+
+                <div class="training">
+                    <div class="training-hd">
+                        <div class="stretch">
+                            <h3 class="hdg-5 hdg-bold darkblue grid-col-8">
+                                21 Hour Family Bridge Mediation Training Including 6 Hour Domestic Violence Training
+                            </h3>
+                            <span class="">Details</span>
+                        </div>
+                    </div>
+                    <div class="training-subhead grid-col-8">
+                        Meets Rule 114 Family Bridge and 6 Hours of Domestic Violence Training CLE credits to be applied for.
+                    </div>
+                    <div class="training-bd">
+                        <div class="grid-row-s">
+                            <div class="grid-col grid-col-5">
+                                <h4 class="hdg-bold">Topics Include:</h4>
+                                <ul class="training-list">
+                                    <li>Conflict resolution and mediation theory</li>
+                                    <li>Mediation skills and techniques</li>
+                                    <li>Components in the mediation process</li>
+                                    <li>Mediator conduct</li>
+                                    <li>Mediation Rules, statutes, and practices</li>
+                                    <li>Mediating in court, community, and school settings</li>
+                                </ul>
+                                <a class="btn">Download PDF</a>
+                            </div>
+                            <div class="grid-col grid-col-5">
+                                <h4 class="hdg-bold">Prices</h4>
+                                <ul class="training-table">
+                                    <li>
+                                        <table class="training-prices">
+                                            <tbody>
+                                                <tr>
+                                                    <td class="training-prices_category">
+                                                        <span class="training-prices_price">$150</span>
+                                                    </td>
+                                                    <td>
+                                                        <a href="#" class="btn">Register &amp; Buy</a>
+                                                    </td>
+                                                    <td>
+                                                        <a href="#" class="btn">Register</a>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div> 
+                </div> 
+
+            </div>
+        </div>
+    </div> 
+
+    <div class="section-wrap section-wrap-about bg-white">
+        <div class="grid-site">
+            <div class="grid-main">
+                <div class="grid-row-s">
+                    <div class="grid-col grid-col-10">
+                        <h2 class="hdg hdg-4 hdg-bold">Scholarships</h2>
+                    </div>
+                </div>
+                <div class="grid-row-s">
+                    <div class="stretch">
+                        <div>
+                            Something to accessible to all
+                        </div>
+                        <div>
+                            <ul class="h-list h-list_scholarships">
+                                <li>
+                                    Phone: 555-555-5555
+                                </li>
+                                <li>
+                                    Email: hello@email.com
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section-wrap section-wrap-about bg-turq">
+        <div class="grid-site">
+            <div class="grid-main">
+                <div class="grid-row-s">
+                    <div class="grid-col grid-col-10">
+                        <h2 class="hdg-4 hdg-bold">Activities Outside of Our Training</h2>
+                    </div>
+                </div>
+                <div class="grid-row-s">
+                    <ul class="training-links">
+                        <li><a href="#">Outside Event Link</a></li>
+                        <li><a href="#">Outside Event Link</a></li>
+                        <li><a href="#">Outside Event Link</a></li>
+                        <li><a href="#">Outside Event Link</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
 
 <?php get_footer(); ?>
