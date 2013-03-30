a2d3068a88286a5429f2ab727bb495798b95c69f
Scott Garrison <sgarriso@nerdery.com>
2013-03-29 23:51:30 -0500

    updated styling to support page

--- wp-content/themes/skeleton-theme/page-support.php
+++ wp-content/themes/skeleton-theme/page-support.php
@@ -47,7 +47,7 @@
                                 2013 Private Contributions
                             </div>
                             <div class="pledge-bd">
-                                $1,897,124 <i class="icn"></i>
+                                $1,897,124 <i class="icn icn-support icn-package"></i>
                             </div>
                             <div class="pledge-ft">
                                 Goal $70,000
@@ -59,36 +59,38 @@
         </div>
     </div>
 
-    <div id="individual-supports" class="section-wrap js-jump-link-target">
+    <div class="section-wrap">
         <div class="grid-site">
             <div class="grid-main">
-                <div class="grid-col grid-col-6">
-                    <div class="notice">
-                        <div class="notice-hd">
-                            <i class="icn" ></i><h2 class="hdg hdg-4 hdg-bold">Notice</h2>
-                        </div>
-                        <div class="notice-subhd">
-                            Double your donation from November 8-15.
-                        </div>
-                        <div class="notice-bd">
-                            All online donations made between November 8-15 will be matched through a $5,000 Board of Director's and Youthprise challenge  grant. Each donor will be entered to win complimentary tickets to the Guthrie Theater.
+                <div class="grid-row-s">
+                    <div class="grid-col grid-col-6">
+                        <div class="notice">
+                            <div class="notice-hd">
+                                <i class="icn icn-support icn-speaker" ></i><h2 class="hdg-inline hdg-4 hdg-bold">Notice</h2>
+                            </div>
+                            <div class="notice-subhd">
+                                Double your donation from November 8-15.
+                            </div>
+                            <div class="notice-bd">
+                                All online donations made between November 8-15 will be matched through a $5,000 Board of Director's and Youthprise challenge  grant. Each donor will be entered to win complimentary tickets to the Guthrie Theater.
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="grid-col grid-col-4">
-                    <i class="icn"></i>
+                    <div class="grid-col grid-col-4">
+                        <i class="icn icn-support icn-donations"></i>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <div id="donate" class="section-wrap bg-turq js-jump-link-target">
+    <div id="donate" class="section-wrap bg-turq">
         <div class="grid-site">
             <div class="grid-main">
                 <div class="grid-col grid-col-4">
                     <div class="donate-online">
                         <div class="donate-online-hd">
-                            <i class="icn" ></i><h2 class="hdg hdg-4 hdg-bold">Donate Online</h2>
+                            <i class="icn icn-support icn-cpu" ></i><h2 class="hdg hdg-4 hdg-bold hdg-invert hdg-inline">Donate Online</h2>
                         </div>
                         <div class="donate-online-bd">
                             Donations are accepted year-round. Donating online is easy and secure.
@@ -101,18 +103,18 @@
                 <div class="grid-col grid-col-6">
                     <div class="donate-mail">
                         <div class="donate-mail-hd">
-                            <i class="icn" ></i><h2 class="hdg hdg-4 hdg-bold">Donate By Mail</h2>
+                            <i class="icn icn-support icn-letter" ></i><h2 class="hdg hdg-4 hdg-bold hdg-invert hdg-inline">Donate By Mail</h2>
                         </div>
                         <div class="donate-mail-bd">
                             <div>
                                 Your donations are welcome by mail.
                             </div>
                             <div>
-                                Download a Donation Form
+                                <a href="#">Download a Donation Form</a>
                             </div>
                         </div>
                         <div class="donate-mail-ft">
-                            <h3 class="">Send Donations to:</h3>
+                            <h3 class="hdg-5 hdg-bold">Send Donations to:</h3>
                             <ul>
                                 <li>
                                     Community Mediation & Restorative Services
@@ -131,12 +133,12 @@
         </div>
     </div>
 
-    <div id="other-ways" class="section-wrap js-jump-link-target">
+    <div id="other-ways" class="section-wrap">
         <div class="grid-site">
             <div class="grid-main">
                 <div class="grid-col grid-col-12">
-                    <h2 class="">Other Ways to Help</h2>
-                    <ul>
+                    <h2 class="hdg-4 hdg-bold">Other Ways to Help</h2>
+                    <ul class="other-ways-list">
                         <li>
                             Donate Stock
                         </li>
@@ -153,6 +155,130 @@
                             Ask your employer to match your contribution
                         </li>
                     </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="individual-supports" class="section-wrap bg-white js-jump-link-target">
+        <div class="grid-site">
+            <div class="grid-main">
+                <h2 class="hdg-4 hdg-bold">Individual Supports</h2>
+                <div class="support-tree">
+                    <div class="support-tree-hd">
+                        <h3 class="hdg-5 hdg-bold">Presidents Circle ($3,000+)</h3>
+                    </div>
+                    <div class="support-tree-bd">
+                        <ul class="support-tree-list">
+                            <li>
+                                Anonymous
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="support-tree">
+                    <div class="support-tree-hd">
+                        <h3 class="hdg-5 hdg-bold">Leader ($1,000 - $2,999)</h3>
+                    </div>
+                    <div class="support-tree-bd">
+                        <ul class="support-tree-list">
+                            <li>
+                                Don Mason (In memory of Judy Mason)
+                            </li>
+                            <li>
+                                Maple Grove Lions Club
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="support-tree">
+                    <div class="support-tree-hd">
+                        <h3 class="hdg-5 hdg-bold">Advocate ($250 - $499)</h3>
+                    </div>
+                    <div class="support-tree-bd">
+                        <ul class="support-tree-list">
+                            <li>
+                                Lee &amp; Barbara Bearmon
+                            </li>
+                            <li>
+                                Larry Brauch
+                            </li>
+                            <li>
+                                Jim Campbell
+                            </li>
+                            <li>
+                                Daksha Chugani
+                            </li>
+                            <li>
+                                Mary Heer-Forsberg
+                            </li>
+                            <li>
+                                Jack Leveille
+                            </li>
+                            <li>
+                                Jonathan Levy
+                            </li>
+                            <li>
+                                Michael Swanson
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="supporting-groups" class="section-wrap js-jump-link-target">
+        <div class="grid-site">
+            <div class="grid-main">
+                <h2 class="hdg-4 hdg-bold">Supporting Groups</h2>
+                <div class="grid-row-s">
+                    <div class="grid-row-5">
+                        <div class="supporting-groups-subhd">
+                            Thank you to all who invest in our community through donations and contracted support.
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="grid-row-s">
+                    <div class="grid-col grid-col-5 supporting-groups-section">
+                        <h3 class="hdg-5 hdg-bold">Contracts</h3>
+                        <ul class="supporting-groups-list">
+                            <li>Fourth Judical District Court</li>
+                            <li>Hennepin County Human Services and Public Health Dept</li>
+                            <li>Youth Intervention Program (Office of Justice Programs)</li>
+                            <li>Fourth Judical District Court</li>
+                            <li>
+                                Municipalities
+                                <ul class="supporting-groups-sublist">
+                                    <li>Brooklyn Center</li>
+                                    <li>Brooklyn Park</li>
+                                    <li>Corcoran</li>
+                                    <li>Crystal</li>
+                                    <li>Golden Valley</li>
+                                    <li>Hopkins</li>
+                                    <li>Maple Grove</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="grid-col grid-col-5 supporting-groups-section-foundations">
+                        <h3 class="hdg-5 hdg-bold">Foundations</h3>
+                        <ul class="supporting-groups-list">
+                            <li>American Bar Association</li>
+                            <li>Golden Valley Human Services Foundation</li>
+                            <li>Minneapolis Foundation (through MN Youth Intervention Progam Association)</li>
+                            <li>Minnesota State Bar Association- ADR Section</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="donor-policy">
+                    <h2 class="hdg-5 hdg-bold">Our Donor Privacy Policy</h2>
+                    <div class="donor-subhead">Please Download our Donor Privacy Policy</div>
+                    <a href="#" class="btn">Donor Privacy Policy</a>
                 </div>
             </div>
         </div>
