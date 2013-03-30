f843be7d9588a52555d126b9695c964d6b62b04f
John Heimkes IV <john@markupisart.com>
2013-03-29 20:25:26 -0500

    killed woocommerce and added events manager

---
+++ wp-content/plugins/events-manager/includes/thumbnails/timthumb-config.php
@@ -0,0 +1,4 @@
+<?php
+//applied a fix https://code.google.com/p/timthumb/issues/detail?id=120#c28
+define('FETCH_LOCAL_URL',true);
+define ('FILE_CACHE_DIRECTORY', '../../../../uploads/em-cache/');
