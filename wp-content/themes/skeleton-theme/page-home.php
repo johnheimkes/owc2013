f3cec45d478c209fcad34693f53c9444b51828b6
Merne William <willi828@gmail.com>
2013-04-28 14:32:08 -0500

    derpa

--- wp-content/themes/skeleton-theme/page-home.php
+++ wp-content/themes/skeleton-theme/page-home.php
@@ -134,7 +134,7 @@
                         </div>
                         <div class="feature-bd feature-bd--flex-box">
                             <?php the_sub_field('home_section_4a_donors_paragraph'); ?>
-                            <img style="margin-top: 10px" src="<?php echo bloginfo('template_url'); ?>/assets/images/standards_cmyk02_2.png" width="100" height="75" alt="" />
+                            <a href="http://www.smartgivers.org/meets_standards_seal" rel="external"><img style="margin-top: 10px" src="<?php echo bloginfo('template_url'); ?>/assets/images/standards_cmyk02_2.png" width="100" height="75" alt="" /></a>
                         </div>
                         <div class="feature-ft">
                             <a href="<?php echo home_url(); ?>/donate" class="btn">More About Donations</a>
