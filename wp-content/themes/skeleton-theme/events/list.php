647c2f8a20157b851434ae07f7d9ab6f98bfff21
John Heimkes IV <john@markupisart.com>
2013-05-01 22:55:31 -0500

    bullshit

--- wp-content/themes/skeleton-theme/events/list.php
+++ wp-content/themes/skeleton-theme/events/list.php
@@ -30,55 +30,6 @@ if ( !defined('ABSPATH') ) { die('-1'); }
 
 			<div class="tribe-events-event-list-meta" itemprop="location" itemscope itemtype="http://schema.org/Place">
 				<table cellspacing="0">
-					<?php if (tribe_is_multiday() || !tribe_get_all_day()): ?>
-					<tr>
-						<td class="tribe-events-event-meta-desc"><?php _e('Start:', 'tribe-events-calendar'); ?></td>
-						<td class="tribe-events-event-meta-value" itemprop="startDate" content="<?php echo tribe_get_start_date(); ?>"><?php echo tribe_get_start_date(); ?></td>
-					</tr>
-					<tr>
-						<td class="tribe-events-event-meta-desc"><?php _e('End:', 'tribe-events-calendar'); ?></td>
-						<td class="tribe-events-event-meta-value" itemprop="endDate" content="<?php echo tribe_get_end_date(); ?>"><?php echo tribe_get_end_date(); ?></td>
-					</tr>
-					<?php else: ?>
-					<tr>
-						<td class="tribe-events-event-meta-desc"><?php _e('Date:', 'tribe-events-calendar'); ?></td>
-						<td class="tribe-events-event-meta-value" itemprop="startDate" content="<?php echo tribe_get_start_date(); ?>"><?php echo tribe_get_start_date(); ?></td>
-					</tr>
-					<?php endif; ?>
-
-					<?php
-						$venue = tribe_get_venue();
-						if ( !empty( $venue ) ) :
-					?>
-					<tr>
-						<td class="tribe-events-event-meta-desc"><?php _e('Venue:', 'tribe-events-calendar'); ?></td>
-						<td class="tribe-events-event-meta-value" itemprop="name">
-							<?php if( class_exists( 'TribeEventsPro' ) ): ?>
-								<?php tribe_get_venue_link( get_the_ID(), class_exists( 'TribeEventsPro' ) ); ?>
-							<?php else: ?>
-								<?php echo tribe_get_venue( get_the_ID() ); ?>
-							<?php endif; ?>
-						</td>
-					</tr>
-					<?php endif; ?>
-					<?php
-						$phone = tribe_get_phone();
-						if ( !empty( $phone ) ) :
-					?>
-					<tr>
-						<td class="tribe-events-event-meta-desc"><?php _e('Phone:', 'tribe-events-calendar'); ?></td>
-						<td class="tribe-events-event-meta-value" itemprop="telephone"><?php echo $phone; ?></td>
-					</tr>
-					<?php endif; ?>
-					<?php if (tribe_address_exists( get_the_ID() ) ) : ?>
-					<tr>
-						<td class="tribe-events-event-meta-desc"><?php _e('Address:', 'tribe-events-calendar'); ?><br />
-						<?php if( get_post_meta( get_the_ID(), '_EventShowMapLink', true ) == 'true' ) : ?>
-							<a class="gmap" itemprop="maps" href="<?php echo tribe_get_map_link(); ?>" title="Click to view a Google Map" target="_blank"><?php _e('Google Map', 'tribe-events-calendar' ); ?></a>
-						<?php endif; ?></td>
-						<td class="tribe-events-event-meta-value"><?php echo tribe_get_full_address( get_the_ID() ); ?></td>
-					</tr>
-					<?php endif; ?>
 					<?php
 						$cost = tribe_get_cost();
 						if ( !empty( $cost ) ) :
@@ -122,32 +73,4 @@ if ( !defined('ABSPATH') ) { die('-1'); }
 
 
 	</div><!-- #tribe-events-loop -->
-	<div id="tribe-events-nav-below" class="tribe-events-nav clearfix">
-
-		<div class="tribe-events-nav-previous"><?php 
-		// Display Previous Page Navigation
-		if( tribe_is_upcoming() && get_previous_posts_link() ) : ?>
-			<?php previous_posts_link( '<span>'.__('&laquo; Previous Events', 'tribe-events-calendar').'</span>' ); ?>
-		<?php elseif( tribe_is_upcoming() && !get_previous_posts_link( ) ) : ?>
-			<a href='<?php echo tribe_get_past_link(); ?>'><span><?php _e('&laquo; Previous Events', 'tribe-events-calendar' ); ?></span></a>
-		<?php elseif( tribe_is_past() && get_next_posts_link( ) ) : ?>
-			<?php next_posts_link( '<span>'.__('&laquo; Previous Events', 'tribe-events-calendar').'</span>' ); ?>
-		<?php endif; ?>
-		</div>
-
-		<div class="tribe-events-nav-next"><?php
-		// Display Next Page Navigation
-		if( tribe_is_upcoming() && get_next_posts_link( ) ) : ?>
-			<?php next_posts_link( '<span>'.__('Next Events &raquo;', 'tribe-events-calendar').'</span>' ); ?>
-		<?php elseif( tribe_is_past() && get_previous_posts_link( ) ) : ?>
-			<?php previous_posts_link( '<span>'.__('Next Events &raquo;', 'tribe-events-calendar').'</span>' ); // a little confusing but in 'past view' to see newer events you want the previous page ?>
-		<?php elseif( tribe_is_past() && !get_previous_posts_link( ) ) : ?>
-			<a href='<?php echo tribe_get_upcoming_link(); ?>'><span><?php _e('Next Events &raquo;', 'tribe-events-calendar'); ?></span></a>
-		<?php endif; ?>
-		</div>
-
-	</div>
-	<?php if ( !empty($hasPosts) && function_exists('tribe_get_ical_link') ): ?>
-		<a title="<?php esc_attr_e('iCal Import', 'tribe-events-calendar'); ?>" class="ical" href="<?php echo tribe_get_ical_link(); ?>"><?php _e('iCal Import', 'tribe-events-calendar'); ?></a>
-	<?php endif; ?>
 </div>
